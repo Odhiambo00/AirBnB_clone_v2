54a130e67bf7ade80440ea1e413e1a227876a9b1
Odhiambo00 <odhiamboelias99@gmail.com>
2023-08-22 12:05:59 +0300

    models init update

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,13 @@
 """
 initialize the models package
 """
-
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from os import getenv
 
 
